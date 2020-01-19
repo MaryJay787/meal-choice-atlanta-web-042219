8a8478caeb461dc658b822db8f8f2b87e563fbcb
MaryJay787 <josephmaryam.mj@gmail.com>
2020-01-19 18:14:26 -0500

    default argument and interpolation practice

--- meal_choice.rb
+++ meal_choice.rb
@@ -3,3 +3,18 @@
 def meal_choice(vegan = "meat")
    "#{vegan}"
 end
+
+cheese = ["kraft", "cheddar", "velveta"].sample
+
+def second_meal_choice(cheese)
+   case cheese
+   when "velveta"
+      puts "The only kind of cheese I like is #{cheese}!"
+   when "kraft"
+      puts "The only kind of cheese I like is #{cheese}!"
+   when "chedder"
+      puts "The only kind of cheese I like is #{cheese}!"
+   end
+end
+
+second_meal_choice(cheese)
